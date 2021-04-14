a8e80443023453bed53d42e5147e9d6f1b9c5d75
carolinesimoes <simoesacaroline@gmail.com>
2021-04-14 18:43:28 -0300

    initial commit

---
+++ src/app/painel-simples/painel-simples.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-simples',
+  templateUrl: './painel-simples.component.html',
+  styleUrls: ['./painel-simples.component.css']
+})
+export class PainelSimplesComponent implements OnInit {
+
+  @Input() titulo;
+  @Input() col;
+  @Input() tipo = "info";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
